6ac96f092b4022714217d00282bc744cb61f4e11
KostyaNike <deras1234oo@gmail.com>
2024-11-27 15:49:25 +0100

    додано логін і реєстрацію.

--- auth_sys/views.py
+++ auth_sys/views.py
@@ -1,10 +1,40 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from .models import CustomUser
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
 
-# Create your views here.
-# class TestUsersView(ListView):
-#     model = CustomUser
-#     context_object_name = 'users'
+def register_view(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('room-list')
+    else: 
+        form = CustomUserCreationForm()
+    return render(request, 'auth_sys/register.html', {'form': form})
 
-#     template_name = 'auth_sys/test.html'
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user=user)
+                # return redirect('room-list')
+            else:
+                messages.error(request, 'Incorrect username or password :(')
+        else:
+            messages.error(request, 'Invalid form submission.')
+
+    form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'auth_sys/login.html', context)
+
+def logout_view(request):
+    pass
