7f4a2f3ce885b708bc63a2c69808cd6363719320
WanilkaGIT <mahlunivan27@gmail.com>
2024-11-27 20:45:09 +0200

    models

--- events_system/models.py
+++ events_system/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Announcement(models.Model):
+    title = models.CharField(max_length=100,null=False)
+    description = models.TextField()
+    date_start = models.DateTimeField(null=True)
+    date_end = models.DateTimeField(null=True)
+    img = models.FileField(upload_to="comments_media/", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+class Event(models.Model):
+    title = models.CharField(max_length=100,null=False)
+    description = models.TextField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    date_start = models.DateTimeField(null=True)
+    date_end = models.DateTimeField(null=True)
+    img = models.FileField(upload_to="comments_media/", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
