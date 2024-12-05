e4273bee40c3617320ad6d1bbfcb832392d8404e
GoodsYT <volodapapana@gmail.com>
2024-12-05 23:47:11 +0200

    оновлення поста на форумi

--- forum_system/urls.py
+++ forum_system/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('category/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
     path('create-topic/', views.TopicCreateView.as_view(), name='create-topic'),
     path('topic/<int:pk>/', views.TopicDetailView.as_view(), name='topic-detail'),
+    path('topic/<int:pk>/edit/', views.update_topic, name='update-topic'),
 ]
