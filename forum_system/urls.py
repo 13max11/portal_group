4e486dc8ac8c4b3578b409f89218d8dbb591fea1
KostyaNike <deras1234oo@gmail.com>
2024-12-12 17:01:28 +0100

    Зробиш ще раз голосування але є помилка яку треба виправити.

--- forum_system/urls.py
+++ forum_system/urls.py
@@ -9,4 +9,11 @@ urlpatterns = [
     path('topic/<int:pk>/', views.TopicDetailView.as_view(), name='topic-detail'),
     path('topic/<int:pk>/edit/', views.update_topic, name='update-topic'),
     path('topic/<int:pk>/delete', views.delete_topic, name='delete-topic'),
+
+    # urls для голосування
+    path('poll/<int:poll_id>/vote/', views.vote_poll, name='vote'),
+    path('topic/<int:topic_id>/create-poll/', views.create_poll, name='create-poll'),
+    path('poll/<int:poll_id>/delete/', views.delete_poll, name='delete-poll'),
+    path('poll/<int:poll_id>/add-option/', views.add_poll_option, name='add-poll-option'),
+    path('poll-option/<int:option_id>/delete/', views.delete_poll_option, name='delete-poll-option'),
 ]
