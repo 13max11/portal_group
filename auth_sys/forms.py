e383a3792cd1e4106ba33937fe2d0e40d4ee866c
WanilkaGit <130846873+WanilkaGit@users.noreply.github.com>
2024-12-14 18:20:12 +0000

    Профіль та Зміна профілю

--- auth_sys/forms.py
+++ auth_sys/forms.py
@@ -5,3 +5,28 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "email")
+
+
+from django import forms
+from django.contrib.auth.models import User
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'phone_number', 'description', 'avatar']
+
+class ChangePasswordForm(forms.Form):
+    current_password = forms.CharField(widget=forms.PasswordInput)
+    new_password = forms.CharField(widget=forms.PasswordInput)
+
+class ChangeAvatarForm(forms.Form):
+    avatar = forms.ImageField()
+
+class ChangeEmailForm(forms.Form):
+    email = forms.EmailField()
+
+class ChangePhoneNumberForm(forms.Form):
+    phone_number = forms.CharField()
+
+class ChangeDescriptionForm(forms.Form):
+    description = forms.CharField(widget=forms.Textarea)
--- auth_sys/urls.py
+++ auth_sys/urls.py
@@ -7,4 +7,13 @@ urlpatterns = [
     path('register/', views.RegistationView.as_view(), name='register'),
     path('login/', LoginView.as_view(template_name='auth_sys/login.html', redirect_authenticated_user = True), name='login'),
     path('logout/', views.logout_view, name='logout'),
+
+
+    path('profile/', views.profile_view, name='profile-view'),
+    path('profile/update/', views.profile_update, name='profile-update'),
+    path('profile/change-password/', views.change_password, name='change-password'),
+    path('profile/change-avatar/', views.change_avatar, name='change-avatar'),
+    path('profile/change-email/', views.change_email, name='change-email'),
+    path('profile/change-phone-number/', views.change_phone_number, name='change-phone-number'),
+    path('profile/change-description/', views.change_description, name='change-description'),
 ]
