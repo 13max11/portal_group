40f2da93d5954c61ea2ad85c215ae923f7c4662f
max <ms20090806@gmail.com>
2024-12-14 19:43:05 +0100

    виправив проблему з регестрацією

--- auth_sys/urls.py
+++ auth_sys/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.AuthPageView.as_view(), name='auth-page'),
-    path('register/', views.RegistationView.as_view(), name='register'),
+    path('register/', views.registration, name='register'),
     path('login/', LoginView.as_view(template_name='auth_sys/login.html', redirect_authenticated_user = True), name='login'),
     path('logout/', views.logout_view, name='logout'),
 ]
