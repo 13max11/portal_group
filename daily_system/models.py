d0dbf5bf4143ed698b736e25f271e24789fc8164
max <ms20090806@gmail.com>
2024-12-17 18:59:40 +0100

    щось там трохи переробив щоб працювало нормально

--- daily_system/models.py
+++ daily_system/models.py
@@ -3,34 +3,25 @@ from django.db import models
 
 class Lesson(models.Model):
     STATUS_CHOICES = [
-        ('missing', 'Missing'),
-        ('present', 'Present'),
+        ('missing', 'Відсутній'),
+        ('present', 'Присутній'),
     ]
     date = models.DateTimeField()
     theme = models.CharField(max_length=80)
-<<<<<<< HEAD
     presence = models.CharField(max_length=27, 
-                    choices=STATUS_CHOICES, default='missing')#відсутність чи присутністьь учня на уроці
-    STATUS_CHOICES = [
-=======
-    presence = models.CharField(max_length=20, choices=[
-        ('present', "Present"),
-        ('missing', "Missing"),
-    ]) #відсутність чи присутністьь учня на уроці
+                choices=STATUS_CHOICES, default='missing')#відсутність чи присутністьь учня на уроці
 
     def __str__(self):
         return f'{self.theme} - {self.date}'
     
     
 class Homework(models.Model):
-    grade = models.IntegerField()
-    homework = models.CharField(max_length=100)
-    status = models.CharField(max_length=20, choices=[
->>>>>>> 301c715cf2637abb8031b0c35d9cbceaf68cceae
-        ('done', "Done"),
-        ('in_progress', "In progress"),
-        ('to_do', "To do"),
+    STATUS_CHOICES = [
+        ('done', "Зроблено"),
+        ('in_progress', "Робиться"),
+        ('to_do', "Треба зробити"),
     ]
+
     grade = models.IntegerField()
     homework = models.CharField(max_length=180)
     status = models.CharField(max_length=27, 
